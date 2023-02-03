d38e2512aa61629d432abfe280c186f158346659
GuruprasadKini <120703432+GuruprasadKini@users.noreply.github.com>
2023-02-03 15:46:59 +0530

    Update LambdaTest.java

--- LambdaTestAutomation/src/main/java/test/LambdaTest.java
+++ LambdaTestAutomation/src/main/java/test/LambdaTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LambdaTest {
-	@Test(timeOut = 30000)
+	@Test(timeOut = 60000)
 	@Parameters({"platform", "browser", "browserVersion"})
 	public void runTest(String platform, String browser, String browserVersion) {
 		LambdaTestScenarios runScenario = new LambdaTestScenarios();
